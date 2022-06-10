6d44fa9beba108316f77fd4ac58f562e848d25c4
YechanChae <chsg2036@gmail.com>
2022-06-11 00:21:55 +0900

    세팅

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
-
 const app = express();
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const multer = require("multer");
+require("dotenv").config();
+
+mongoose.connect("mongodb://localhost/hanghae99_week4HW", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
 
 router.get("/", (req, res) => {
   res.send("Hi!");
@@ -9,5 +21,5 @@ router.get("/", (req, res) => {
 app.use("/api", express.json(), router);
 
 app.listen(8080, () => {
-  console.log("서버가 켜졌어어요.!!!!!!!!!!");
+  console.log("서버가 켜졌어어요.");
 });
