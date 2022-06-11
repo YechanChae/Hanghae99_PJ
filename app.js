c368ac9a85a477a7c18961e3cefa041f761fc4e9
howooke <howooke@gmail.com>
2022-06-11 23:59:32 +0900

    커밋

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
-const jwt = require("jsonwebtoken");
-const Joi = require("joi");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const multer = require("multer");
@@ -15,11 +12,18 @@ mongoose.connect("mongodb://localhost/hanghae99_week4HW", {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
-router.get("/", (req, res) => {
+const usersRouter = require("./routes/users")
+const authMiddleware = require("./routes/auth-middleware")
+
+
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
   res.send("Hi!");
 });
-app.use("/api", express.json(), router);
+app.use("/api", express.urlencoded({ extended: false }), [usersRouter])
 
 app.listen(8080, () => {
-  console.log("서버가 켜졌어어요.");
+  console.log("서버가 켜졌어요.");
 });
